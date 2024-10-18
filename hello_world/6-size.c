590f86fce4ced7e0066cf071c9cac25de1062141
haug77 <10037@holbertonstudents.com>
2024-10-18 16:44:09 +0000

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,17 +11,17 @@
 int main(void)
 {
 #if defined(__x86_64__) || defined(_M_X64)
-printf("Size of a char: 1 byte(s)\n
-Size of an int: 4 byte(s)\n
-Size of a long int: 8 byte(s)\n
-Size of a long long int: 8 byte(s)\n
-Size of a float: 4 byte(s)\n");
+printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int: 8 byte(s)\n");
+printf("Size of a long long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)\n");
 #elif defined(__i386__) || defined(_M_IX86)
-printf("Size of a char: 1 byte(s)\n
-Size of an int: 4 byte(s)\n
-Size of a long int: 4 byte(s)\n
-Size of a long long int: 8 byte(s)\n
-Size of a float: 4 byte(s)\n");
+printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int: 4 byte(s)\n");
+printf("Size of a long long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)\n");
 #else printf("Arquitectura desconocida.\n");
 #endif
 
