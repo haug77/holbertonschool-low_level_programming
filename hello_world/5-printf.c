7516fb9705c5eb2ee6ed503913c5ac036e196b8d
haug77 <10037@holbertonstudents.com>
2024-10-18 15:01:54 +0000

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Punto de entrada del programa
+ *
+ * Funcion - Imprimir "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: 0 si se ejecuta correctamente
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,"\n);
+return (0);
+}
