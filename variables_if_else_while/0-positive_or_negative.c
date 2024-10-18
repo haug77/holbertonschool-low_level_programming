fb5d9f7fbca1f71210caf9b605f5a06bbdaec045
haug77 <10037@holbertonstudents.com>
2024-10-18 17:28:22 +0000

    0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Punto de entrada del programa
+ *
+ * Funcion - Declarar si n es positivo, negativo o cero
+ *
+ * Return: 0 si se ejecuta correctamente
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if(n>0)
+	printf("is positive");
+	else if(n<0)
+	printf("is negative");
+	else
+	printf("is zero");
+
+	return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+uso de if, else, while
