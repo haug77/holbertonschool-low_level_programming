e3cfb7fadedd7f3cae8909f869267c6b917ca263
haug77 <10037@holbertonstudents.com>
2024-10-18 14:54:24 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,14 +3,14 @@
 /**
  * main - Punto de entrada del programa
  *
- * Funcion - Imprimir "Programming is like building a multilingual puzzle"
+ * Funcion - Imprimir "\"Programming is like building a multilingual puzzle"
  *
  * Return: 0 si se ejecuta correctamente
  */
 
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
