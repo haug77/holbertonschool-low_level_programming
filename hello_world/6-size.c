f4747a6b7ca9454ec98c9692da3e7ca98a61bb47
haug77 <10037@holbertonstudents.com>
2024-10-18 16:37:14 +0000

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Punto de entrada del programa
+ *
+ * Funcion - Imprimir el tama√o de varios tipos de datos
+ *
+ * Return: 0 si se ejecuta correctamente
+ */
+
+int main(void)
+{
+#if defined(__x86_64__) || defined(_M_X64)
+printf("Size of a char: 1 byte(s)\n
+Size of an int: 4 byte(s)\n
+Size of a long int: 8 byte(s)\n
+Size of a long long int: 8 byte(s)\n
+Size of a float: 4 byte(s)\n");
+#elif defined(__i386__) || defined(_M_IX86)
+printf("Size of a char: 1 byte(s)\n
+Size of an int: 4 byte(s)\n
+Size of a long int: 4 byte(s)\n
+Size of a long long int: 8 byte(s)\n
+Size of a float: 4 byte(s)\n");
+#else printf("Arquitectura desconocida.\n");
+#endif
+
+return (0);
+}
