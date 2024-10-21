586e0d7e08e506c3b3431f4796996dbc5076cfc6
haug77 <10037@holbertonstudents.com>
2024-10-21 13:47:35 +0000

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ n = rand() - RAND_MAX / 2;
 lastDig = n % 10;
 
 if (lastDig > 5)
-printf("%d is greater than 5\n", n);
+printf("Last digit of %d is greater than 5\n", n);
 else if (lastDig == 0)
-printf("%d is 0\n", n);
+printf("Last digit of %d is 0\n", n);
 else
-printf("%d is less than 6 and not 0\n", n);
+printf("Last digit of %d is less than 6 and not 0\n", n);
 
 return (0);
 }
