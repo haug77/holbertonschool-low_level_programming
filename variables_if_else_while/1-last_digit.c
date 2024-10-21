f87cd211281b9b1772a0381d0ecab1e22fe4cd24
haug77 <10037@holbertonstudents.com>
2024-10-21 13:42:48 +0000

    1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Punto de entrada del programa
+ *
+ * Funcion - Declarar si n es positivo, negativo o cero
+ *
+ * Return: 0 si se ejecuta correctamente
+ */
+
+int main(void)
+{
+int n;
+int lastDig;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastDig = n % 10;
+
+if (lastDig > 5)
+printf("%d is greater than 5\n", n);
+else if (lastDig == 0)
+printf("%d is 0\n", n);
+else
+printf("%d is less than 6 and not 0\n", n);
+
+return (0);
+}
