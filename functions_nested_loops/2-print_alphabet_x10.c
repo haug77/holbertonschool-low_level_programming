7275bbe5ad0b730ae4eb4840e96b74c26893c736
haug77 <10037@holbertonstudents.com>
2024-10-24 13:46:24 +0000

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,12 @@ void print_alphabet_x10(void)
 {
 int line, abc;
 
-for (line = 0; line <= 10; line++)
+for (line = 0; line < 10; line++)
 {
 for (abc = 'a'; abc <= 'z'; abc++)
 {
 _putchar(abc);
 }
-}
 _putchar('\n');
+}
 }
