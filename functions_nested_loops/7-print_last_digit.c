221020a1e3e50a72b9adb29be6f425df2af53560
haug77 <10037@holbertonstudents.com>
2024-10-24 17:33:28 +0000

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,16 @@
 
 /**
  * print_last_digit - imprime el ultimo digito de un numero
- * @u: parametro, el numero que la funcion tomara
+ * @n: parametro, el numero que la funcion tomara
  * Return: retorna el valor del ultimo digito
  */
-int print_last_digit(int u)
+int print_last_digit(int n)
 {
-_putchar(u % 10);
-return (u % 10);
+int last_digit = n % 10;
+
+if (last_digit < 0)
+last_digit = -last_digit;
+
+_putchar(last_digit);
+return (last_digit);
 }
