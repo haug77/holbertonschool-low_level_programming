a6ef0cf34ff4710552dc5cc4a7ae774e29a734a3
haug77 <10037@holbertonstudents.com>
2024-10-25 14:26:06 +0000

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * times_table - imprime las tablas del 0 al 9
+ */
+void times_table(void)
+{
+int t, num, c;
+for (t = 0; t < 9; t++)
+{
+for (num = 0; num < 9; num++)
+{
+c = t * num;
+_putchar(c);
+}
+}
+}
