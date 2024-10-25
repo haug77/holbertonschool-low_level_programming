7f1accc54c4d4952fb297f611f951aa015042369
haug77 <10037@holbertonstudents.com>
2024-10-25 16:51:46 +0000

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - funcion para sumar dos numeros enteros
+ * @a, @b - parametros, los numeros a sumar
+ * Return - 0 si no hay errores
+ */
+int add(int a, int b)
+{
+int suma = a + b;
+printf("%d\n", suma);
+return (0);
+}
