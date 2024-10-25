6b7179727686cb6311edbd5ad68bbe6eeb96e671
haug77 <10037@holbertonstudents.com>
2024-10-25 14:28:40 +0000

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,9 +6,9 @@
 void times_table(void)
 {
 int t, num, c;
-for (t = 0; t < 9; t++)
+for (t = '0'; t < '9'; t++)
 {
-for (num = 0; num < 9; num++)
+for (num = '0'; num < '9'; num++)
 {
 c = t * num;
 _putchar(c + '0');
