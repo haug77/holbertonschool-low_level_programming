d4c7f8f3acea0972380b7721caef7edb77c8781e
haug77 <10037@holbertonstudents.com>
2024-10-28 17:58:19 +0000

    work

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - imprime 10 veces los numeros del 0 al 14
+ */
+void more_numbers(void)
+{
+	int h, i, j;
+
+	for (h = 48; h <= 57 + 49; h++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			if (i == 57)
+			{
+				for (j = 0; j <= 53; j++)
+					j = i + j;
+				_putchar(i);
+				_putchar(j);
+			}
+		}
+		_putchar('\n');
+	}
+}
