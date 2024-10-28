8196e52bcb678e116f1d245603b62d950eafe4d4
haug77 <10037@holbertonstudents.com>
2024-10-28 18:02:38 +0000

    work

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,12 @@ void more_numbers(void)
 
 		for (i = 48; i <= 57; i++)
 		{
+			_putchar(i);
 			if (i == 57)
 			{
 				for (j = 0; j <= 53; j++)
 					j = i + j;
-				_putchar(i);
 				_putchar(j);
 			}
 		}
-		_putchar('\n');
 }
