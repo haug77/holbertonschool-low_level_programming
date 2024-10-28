39f7be18538d217f850f5a3f732032653344f6ea
haug77 <10037@holbertonstudents.com>
2024-10-28 17:11:16 +0000

    work

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - recibe dos enteros y los multiplica
+ * @a: entero a multiplicar por la funcion
+ * @b: entero a multiplicar por la funcion
+ * Return: resultado de la multiplicacion
+ */
+int mul(int a, int b)
+{
+	int mul = a * b;
+return (mul);
+}
