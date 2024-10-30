505f2bbbc9d5e830708f07cb551e1cef41cc9c3e
haug77 <10037@holbertonstudents.com>
2024-10-30 15:52:02 +0000

    work

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Imprimir los numeros del 1 al 100, cambiando los multiplos de 3 y 5
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", a);
+	}
+	return (0);
+}
