f9852968eeb0f8fb6d6a99640bc33ce5deefccfb
haug77 <10037@holbertonstudents.com>
2024-10-30 17:06:48 +0000

    work

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - imprime un triangulo
+ * @size: cantidad de # que formaran la base del triangulo
+ */
+void print_triangle(int size)
+{
+	int y, z;
+
+	if (size > 0)
+	{
+		for (y = 0; y < size; y++)
+		{
+			for (z = size; z > 0; z--)
+				_putchar(35);
+
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
