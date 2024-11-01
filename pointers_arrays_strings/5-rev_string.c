93e6717e1dc0aeaaf5a8199f42840fe517ec36f5
haug77 <10037@holbertonstudents.com>
2024-11-01 18:17:26 +0000

    work

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - Invierte un string
+ * @s: string ingresado para que la funcion imprima
+ */
+void rev_string(char *s)
+{
+	int l = 0;
+
+
+	while (s[l] != '\0')
+		l++;
+	l--;
+	while (l >= 0)
+	{
+		*s = s[l];
+		l--;
+	}
+}
