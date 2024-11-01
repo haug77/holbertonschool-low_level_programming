ee0f6d7f3654c52813f3e7cfbb59685bb8e6d13e
haug77 <10037@holbertonstudents.com>
2024-11-01 17:31:56 +0000

    work

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - Imprime un string invertido
+ * @s: string ingresado para que la funcion imprima
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+		l++;
+	while (s[l] == 0)
+	{
+		_putchar(l);
+		l--;
+	}
+	_putchar('\n');
+}
