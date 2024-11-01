d4836118641a482eb172e589c4430404a807e0d8
haug77 <10037@holbertonstudents.com>
2024-11-01 15:55:16 +0000

    work

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - Imprime un string
+ * @str: string ingresado para que la funcion imprima
+ */
+void _puts(char *str)
+{
+	int l = 0;
+
+	while (str[l] != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+}
