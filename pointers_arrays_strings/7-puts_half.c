1efcf199f6fcf05ad38390ada270051b3e3b8c98
haug77 <10037@holbertonstudents.com>
2024-11-03 13:48:01 +0000

    work

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - Imprimr la segunda mitad de un string
+ * @str: string ingresado para que la funcion imprima
+ */
+void puts_half(char *str)
+{
+	int a = 0;
+	int b, c;
+
+	while (str[a] != '\0')
+		a++;
+
+	if (a % 2 == 0)
+		b = a / 2;
+
+	else
+		b = (a - 1) / 2;
+
+	for (c = b; c < a; c++)
+		_putchar(str[c]);
+
+	_putchar('\n');
+}
